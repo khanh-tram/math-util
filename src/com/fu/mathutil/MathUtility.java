8b5fb12bc82b22d47a2700cc1d2129e26a1dd370
tramlun <khanhtram1812@gmail.com>
2021-03-25 16:28:46 +0700

    RECURSION UPDATED & GREEN 4EVER AGAIN

--- src/com/fu/mathutil/MathUtility.java
+++ src/com/fu/mathutil/MathUtility.java
@@ -13,15 +13,23 @@ public class MathUtility {
     public static long getFactorial(int n){
         if(n<0 || n>20)
             throw new IllegalArgumentException("n must be between 0..20");
+        //sửa code, sửa hàm giai thừa cho gọn, dùng đệ quy
+        if(n==0 || n==1)
+            return 1; //0!=1!=1 tính luôn cho nhanh
+        return n * getFactorial(n-1);
+        //n!=  n *     (n-1)!
+        //đệ quy, vòng lại chính mình với qui mô #
+        //recursion. Làm khéo nếu ko nó di mãi mãi
+        //STACKOVERFLOW.COM
         
         //sống sót đến đây thì n=0,1, ...20
-        if(n==0 || n==1)
-            return 1; //0!,1!=1
-        
-        long result= 1;//XANH LOCAL VA SERVER
-        for(int i=2; i<=n; i++)
-            result *= i;
-        
-        return result; //nhan tu 2 3 4 5 6 7....
+//        if(n==0 || n==1)
+//            return 1; //0!,1!=1
+//        
+//        long result= 1;//XANH LOCAL VA SERVER
+//        for(int i=2; i<=n; i++)
+//            result *= i;
+//        
+//        return result; //nhan tu 2 3 4 5 6 7....
     }
 }
